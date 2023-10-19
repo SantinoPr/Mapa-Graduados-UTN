407001d8f75995ebc1cd1f0212afaa48983da17e
Santino <santino-albarello@hotmail.com>
2023-10-18 22:01:45 -0300

    mejora en los marcadores del mapa

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -3,22 +3,65 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from 'leaflet';
-function map() {
+import { useState } from "react";
+function Map() {
+
+  const [marker, setMarker]=useState(null);
+  const [country, setCountry]=useState(null);
+
+ 
+
+    async function geoCodificar(ciudad) {
+      const apiUrl = "https://nominatim.openstreetmap.org/search?q=" + ciudad + "&format=json&limit=1";
+         await fetch(apiUrl)
+         .then((response) => response.json())
+         .then((data) => {
+          this.longitud = data[0].lon;
+          this.latitud = data[0].lat;
+          const latlon = this.latitud + " " + this.longitud;
+          console.log(latlon);
+          return latlon;
+        })
+        .catch((err) => console.log(err));
+    }
+
+    async function ObtenerLatitudLongitud() {
+    try {
+      let codificar = geoCodificar(country.value);
+      let campos = codificar.split(" ");
+      const latitud=campos[0];
+      const longitud=campos[1];
+      
+      setMarker(
+        <Marker position={[latitud, longitud]}>
+            <Popup>
+              UwU <br /> Easily customizable.
+            </Popup>
+      </Marker>
+      )
+      
+    } catch (error) {
+      console.error("Error", error);
+    }
+  }
+
+
+
   return (
     <div className="container">
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Localidad
-        </label>
-        <input id="country" className="form-control" type="text" placeholder="Localidad" aria-label="default input example"
-        ></input>
-        <div id="emailHelp" className="form-text">
-          Ingrese la localidad a buscar
-        </div>
+    <div className="mb-3">
+      <label htmlFor="exampleInputEmail1" className="form-label">
+        Localidad
+      </label>
+      <input id="country" className="form-control" type="text"placeholder="Localidad" value={country} aria-label="default input example"
+       onChange={(e) => setCountry(e.target.value)} // Actualiza el estado con el valor del input
+      ></input>
+      <div id="emailHelp" className="form-text">
+        Ingrese la localidad a buscar
       </div>
-      <input id="btnSearch" className="btn btn-primary" type="button" value="Buscar" onClick={obtenerLatitudLongitud(document.getElementById('country'))}></input>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="map">
+    </div>
+    <input id="btnSearch" className="btn btn-primary" type="button" value="Buscar" onClick={ObtenerLatitudLongitud} ></input>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
@@ -27,38 +70,28 @@ function map() {
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        {marker}
       </MapContainer>
     </div>
   );
-}
-async function geoCodificar(ciudad) {
-  this.ciudad = ciudad;
-  const apiUrl =
-    "https://nominatim.openstreetmap.org/search?q=" + ciudad + "&format=json&limit=1";
-     await fetch(apiUrl)
-     .then((response) => response.json())
-     .then((data) => {
-      this.longitud = data[0].lon;
-      this.latitud = data[0].lat;
-      const latlon = this.latitud + " " + this.longitud;
-      console.log(latlon);
-      return latlon;
-    })
-    .catch((err) => console.log(err));
-}
-async function obtenerLatitudLongitud(country) {
+  }
+
+/*async function ObtenerLatitudLongitud(country) {
   try {
-    let map =L.map('.map')
     let codificar = await geoCodificar(country.value);
     let campos = codificar.split(" ");
     const latitud=campos[0];
     const longitud=campos[1];
-    L.marker([latitud, longitud]).addTo(map)
-       .bindPopup('holi')
-       .openPopup(); 
+    return(
+    <Marker position={[latitud, longitud]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+    </Marker>
+    )
   } catch (error) {
     console.error("Error", error);
   }
-}
+}*/
 
-export default map;
+export default Map;
