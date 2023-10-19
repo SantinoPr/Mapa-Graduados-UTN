41a71cd285491c3a29b0cf3d7030b550953f65a6
Santino <santino-albarello@hotmail.com>
2023-10-19 02:37:14 -0300

    funcionando geocodficacion y marcar mapa

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -7,37 +7,37 @@ import { useState } from "react";
 function Map() {
 
   const [marker, setMarker]=useState(null);
-  const [country, setCountry]=useState(null);
-
- 
-
-    async function geoCodificar(ciudad) {
-      const apiUrl = "https://nominatim.openstreetmap.org/search?q=" + ciudad + "&format=json&limit=1";
-         await fetch(apiUrl)
-         .then((response) => response.json())
-         .then((data) => {
-          this.longitud = data[0].lon;
-          this.latitud = data[0].lat;
-          const latlon = this.latitud + " " + this.longitud;
-          console.log(latlon);
-          return latlon;
-        })
-        .catch((err) => console.log(err));
-    }
+  const [country, setCountry]=useState('');
 
     async function ObtenerLatitudLongitud() {
-    try {
-      let codificar = geoCodificar(country.value);
-      let campos = codificar.split(" ");
-      const latitud=campos[0];
-      const longitud=campos[1];
-      
-      setMarker(
-        <Marker position={[latitud, longitud]}>
-            <Popup>
-              UwU <br /> Easily customizable.
-            </Popup>
-      </Marker>
+      try {
+
+        let latitud;
+        let longitud;
+
+         const apiUrl = "https://nominatim.openstreetmap.org/search?q="+country+"&format=json&limit=1";
+         await fetch(apiUrl)
+         .then((response) =>{
+          if(!response.ok){
+            throw new Error('La solicitud no pude efectuarse')
+          }
+          return response.json()
+         })
+         .then((data) => {
+          longitud = data[0].lon;
+          latitud = data[0].lat;
+          const latlon = this.latitud + " " + this.longitud;
+          console.log(latlon);
+        })
+        .catch((err) => console.log(err));     
+        
+        
+         setMarker(
+           <Marker position={[latitud, longitud]}>
+              <Popup>
+                UwU <br /> Easily customizable.
+              </Popup>
+           </Marker>
       )
       
     } catch (error) {
@@ -61,7 +61,7 @@ function Map() {
       </div>
     </div>
     <input id="btnSearch" className="btn btn-primary" type="button" value="Buscar" onClick={ObtenerLatitudLongitud} ></input>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="map">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="map">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
